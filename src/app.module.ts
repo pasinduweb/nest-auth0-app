37d5f37a3e122ac3ccecd6ecc14071a3a6a3fa2f
Pasindu Manaranjana <algopasindu@gmail.com>
2025-10-27 16:24:55 +0530

    add UsersModule with User entity reg

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getTypeOrmConfig } from './config/typeorm.config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { getTypeOrmConfig } from './config/typeorm.config';
         getTypeOrmConfig(configService),
       inject: [ConfigService],
     }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
